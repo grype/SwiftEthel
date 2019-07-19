94792a96d537e1c1d2c2fd4cd08d95a14e75d16f
Pasha <pasha@2vega.com>
2019-07-18 18:11:08 -0700

    Initial Commit

---
+++ SwiftEthel/SwiftEthel.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEthel.h
+//  SwiftEthel
+//
+//  Created by Pavel Skaldin on 7/18/19.
+//  Copyright © 2019 Pavel Skaldin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEthel.
+FOUNDATION_EXPORT double SwiftEthelVersionNumber;
+
+//! Project version string for SwiftEthel.
+FOUNDATION_EXPORT const unsigned char SwiftEthelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEthel/PublicHeader.h>
+
+
