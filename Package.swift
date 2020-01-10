9f2dafa9962d8a914c741e3accf4495e5dc6d6e0
Pasha <pasha@2vega.com>
2020-01-09 18:14:06 -0800

    Pull up dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Ethel",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,8 +19,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/grype/SwiftBeacon", from: "1.0.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.8.0"),
+        .package(url: "https://github.com/grype/SwiftBeacon", .branch("master")),
+        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.12.0"),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
     ],
     targets: [
