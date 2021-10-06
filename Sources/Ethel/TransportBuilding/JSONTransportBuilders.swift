74e61babd1f410ac07e3c6ea2e34fabfca90177f
Pasha <pasha@2vega.com>
2021-10-05 18:34:54 -0700

    Adds EndodeJSON builder
    - Adds optional encoder and decoder instances
    - Rethrows errors
    - Add init block to decoder so that we can add extra logic for materializing objects - this is meant to initialize those instances, if needed

--- Sources/Ethel/TransportBuilding/JSONTransportBuilders.swift
+++ Sources/Ethel/TransportBuilding/JSONTransportBuilders.swift
@@ -1,27 +1,68 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Pavel Skaldin on 10/1/21.
 //
 
-import Foundation
 import Beacon
+import Foundation
+import AnyCodable
 
 /**
- Sets up transport with a content reader that attempts to materialize JSON data into instance of associated type.
+ Sets up transport with a content reader that materializes JSON data into instance of associated type.
  */
 public struct DecodeJSON<T: Decodable>: TransportBuilding {
+    typealias InitBlock = (T) -> Void
+
+    var decoder: JSONDecoder
+    
+    var initBlock: InitBlock?
+
+    init(decoder aDecoder: JSONDecoder = JSONDecoder(), init aBlock: InitBlock? = nil) {
+        decoder = aDecoder
+        initBlock = aBlock
+    }
+
     public func apply(to aTransport: Transport) {
         aTransport.contentReader = { data throws -> T? in
             var result: T?
             do {
-                result = try JSONDecoder().decode(T.self, from: data)
+                result = try decoder.decode(T.self, from: data)
+                initBlock?(result!)
             }
             catch {
                 emit(error: error)
+                throw error
             }
             return result
+        }
+    }
+}
+
+/**
+ Sets up transport with a content writer that serializes objects into JSON.
+ */
+public struct EncodeJSON<T: Encodable>: TransportBuilding {
+    var encoder: JSONEncoder
+    
+    init(encoder anEncoder: JSONEncoder = JSONEncoder()) {
+        encoder = anEncoder
+    }
+    
+    public func apply(to aTransport: Transport) {
+        aTransport.contentWriter = { (content) throws -> Data? in
+            var data: Data?
+            guard let encodableContent = content as? Encodable else { return data }
+            let value = AnyEncodable(encodableContent)
+            do {
+                data = try JSONEncoder().encode(value)
+            }
+            catch {
+                emit(error: error)
+                throw error
+            }
+            return data
         }
     }
 }
