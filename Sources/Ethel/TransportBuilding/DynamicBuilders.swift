cea3b92e233928b63b9cc3b69f7aaad9fd83429d
Pasha <pasha@2vega.com>
2021-10-22 19:58:03 -0700

    Add GetContext() builder

--- Sources/Ethel/TransportBuilding/DynamicBuilders.swift
+++ Sources/Ethel/TransportBuilding/DynamicBuilders.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Pavel Skaldin on 10/4/21.
 //
@@ -10,12 +10,32 @@ import Foundation
 /**
  Evaluates a block, passing it a transport object as the sole argument.
  */
-struct Eval : TransportBuilding {
+struct Eval: TransportBuilding {
     var block: (Transport)->Void
+
     init(_ aBlock: @escaping (Transport)->Void) {
         block = aBlock
     }
+
     func apply(to aTransport: Transport) {
         block(aTransport)
+    }
+}
+
+/**
+ Evaluates a block, passing it current execution context.
+ */
+struct GetContext: TransportBuilding {
+    var queue: DispatchQueue
+    var block: (Context)->Void
+
+    init(queue aQueue: DispatchQueue, block aBlock: @escaping (Context)->Void) {
+        queue = aQueue
+        block = aBlock
+    }
+
+    func apply(to aTransport: Transport) {
+        guard let context = queue.getSpecific(key: CurrentContext) else { return }
+        block(context)
     }
 }
