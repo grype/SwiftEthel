52d16b0c8acccd36550f187ed54931a9ce9c421c
Pasha <pasha@2vega.com>
2022-01-06 17:44:11 -0800

    Add progress monitoring to Upload and Download transport builders

--- Sources/Ethel/TransportBuilding/ContentBuilders.swift
+++ Sources/Ethel/TransportBuilding/ContentBuilders.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import SwiftAnnouncements
+
+public typealias ContentTransferProgressBlock = (Progress, Transport) -> Void
 
 /**
  Configures transport with block-based content reader
@@ -20,7 +23,6 @@ public struct Read<T>: TransportBuilding {
         aTransport.contentReader = block
     }
 }
-
 
 /**
  Configures transport with block-based content writer
@@ -38,39 +40,87 @@ public struct Write: TransportBuilding {
 
 /**
  Configures transport for downloading response to a local file given by a URL.
- 
+
  Typically this requires a GET HTTP method.
  */
 public struct Download: TransportBuilding {
-    var url: URL
-    public init(to aUrl: URL) {
+    public var url: URL
+    public var progressBlock: ContentTransferProgressBlock?
+
+    public init(to aUrl: URL, progress aProgressBlock: ContentTransferProgressBlock? = nil) {
         url = aUrl
+        progressBlock = aProgressBlock
     }
+
     public func apply(to aTransport: Transport) {
         aTransport.requestType = .download(url)
+        subscribe(to: aTransport)
+    }
+
+    func subscribe(to aTransport: Transport) {
+        guard let progressBlock = progressBlock else {
+            return
+        }
+        var subscription: Subscription<Transport.Announcement>!
+        subscription = aTransport.announcer.when(Transport.Announcement.self, subscriber: nil) { announcement, _ in
+            switch announcement {
+            case Transport.Announcement.downloadProgressed:
+                guard let progress = aTransport.downloadProgress else { return }
+                progressBlock(progress, aTransport)
+            case Transport.Announcement.taskEnded:
+                aTransport.announcer.remove(subscription: subscription)
+            default:
+                break
+            }
+        }
     }
 }
 
 /**
  Configures transport for uploading content.
- 
+
  Typically this is going to be a PUT or a POST.
  */
 public struct Upload: TransportBuilding {
     var url: URL!
     var data: Data!
-    public init(fromURL aUrl: URL) {
+    public var progressBlock: ContentTransferProgressBlock?
+
+    public init(fromURL aUrl: URL, progress aProgressBlock: ContentTransferProgressBlock? = nil) {
         url = aUrl
+        progressBlock = aProgressBlock
     }
-    public init(data aData: Data) {
+
+    public init(data aData: Data, progress aProgressBlock: ContentTransferProgressBlock? = nil) {
         data = aData
+        progressBlock = aProgressBlock
     }
+
     public func apply(to aTransport: Transport) {
         if let url = url {
             aTransport.requestType = .uploadFile(url)
         }
         else if let data = data {
             aTransport.requestType = .uploadData(data)
+        }
+        subscribe(to: aTransport)
+    }
+
+    func subscribe(to aTransport: Transport) {
+        guard let progressBlock = progressBlock else {
+            return
+        }
+        var subscription: Subscription<Transport.Announcement>!
+        subscription = aTransport.announcer.when(Transport.Announcement.self, subscriber: nil) { announcement, _ in
+            switch announcement {
+            case Transport.Announcement.downloadProgressed:
+                guard let progress = aTransport.uploadProgress else { return }
+                progressBlock(progress, aTransport)
+            case Transport.Announcement.taskEnded:
+                aTransport.announcer.remove(subscription: subscription)
+            default:
+                break
+            }
         }
     }
 }
