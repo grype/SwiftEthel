3d61f46c937acce761b1e2400a8b62e6f67fdff5
Pasha <pasha@2vega.com>
2022-07-07 18:38:47 -0700

    Fix tests

--- Tests/EthelTests/Test Cases/RequestTransportBuilderTests.swift
+++ Tests/EthelTests/Test Cases/RequestTransportBuilderTests.swift
@@ -19,19 +19,25 @@ class RequestTransportBuilderTests: ClientTestCase {
         super.setUp()
         query = URLQueryItem(name: "aQuery", value: "aValue")
         secondaryQuery = URLQueryItem(name: "aSecondaryQuery", value: "aSecondaryValue")
+        client.prepare().apply(to: transport)
+    }
+    
+    func testRequest() {
+        let url = URL(string: "http://example.com/i-changed-this")!
+        let builder = Request(url)
+        builder.apply(to: transport)
+        expect(self.transport.request?.url).to(equal(url))
     }
     
     func testAddQuery() {
         let builder = AddQuery(query)
-        let transport = client.createTransport()
         builder.apply(to: transport)
-        expect(transport.request?.url?.query).to(equal("\(query.name)=\(query.value!)"))
+        expect(self.transport.request?.url?.query).to(equal("\(query.name)=\(query.value!)"))
     }
     
     func testAddAllQueries() {
         let builder = AddAllQueries([query, secondaryQuery])
-        let transport = client.createTransport()
         builder.apply(to: transport)
-        expect(transport.request?.url?.query).to(equal("\(query.name)=\(query.value!)&\(secondaryQuery.name)=\(secondaryQuery.value!)"))
+        expect(self.transport.request?.url?.query).to(equal("\(query.name)=\(query.value!)&\(secondaryQuery.name)=\(secondaryQuery.value!)"))
     }
 }
